b071d065531ef015d3e597cc1457fcbecd56fb08
dave <dave@daves-MacBook-Pro.local>
2016-12-28 11:36:42 -0800

    Initial work on application functionality

---
+++ src/containers/jobApp/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const JobApp = () => (
+  <div>
+    <h1>Job App</h1>
+  </div>
+);
+
+export default JobApp;
---
+++ src/reducers/applications.reducer.js
@@ -0,0 +1,12 @@
+const initialState = [
+{ href: 'https://www.google.com/jobs', notes: 'great work environment' },
+{ href: 'https://www.google.com/jobs', notes: 'great work environment' },
+{ href: 'https://www.google.com/jobs', notes: 'great work environment' },
+{ href: 'https://www.google.com/jobs', notes: 'great work environment' },
+];
+
+function applications(state = initialState) {
+  return state;
+}
+
+export default applications;
